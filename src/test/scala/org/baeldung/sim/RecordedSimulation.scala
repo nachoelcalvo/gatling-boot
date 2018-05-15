809a76b9413cf3a98ea3c9afd1a360cc63b0b08a
Eugen Paraschiv <hanriseldon@gmail.com>
2018-05-15 14:18:01 +0300

    final scenario

--- src/test/scala/org/baeldung/sim/RecordedSimulation.scala
+++ src/test/scala/org/baeldung/sim/RecordedSimulation.scala
@@ -2,42 +2,57 @@ package org.baeldung.sim
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.http.request.builder.RequestBuilder
 import jodd.util.RandomString
+import org.slf4j.LoggerFactory
 
 import scala.util.Random
 
 class RecordedSimulation extends Simulation {
-
-  def newIsbn() = Random.nextInt(Integer.MAX_VALUE)
-
-  def newTitle() = RandomString.getInstance().randomAlpha(10)
+  val logger = LoggerFactory.getLogger(classOf[Nothing])
 
   val httpProtocol = http
     .baseURL("http://localhost:8080")
     .acceptHeader("application/hal+json")
 
+  val feeder = Iterator.continually(Map("newIsbn" -> (newIsbn()), "newTitle" -> (newTitle())))
 
-  val scn = scenario("RecordedSimulation")
-    .exec(http("get_root")
-      .get("/"))
+  val scn = scenario("RecordedSimulation").feed(feeder)
+    .exec(
+      http("get_root")
+        .get("/")
+        .check(status.is(200))
+    )
     .pause(1)
 
     .exec(http("get_books")
-      .get("/books"))
+      .get("/books")
+      .check(status.is(200))
+    )
     .pause(1)
 
-    .exec(http("create_book")
-      .post("/books")
-      .header("Content-Type", "application/json")
-      // .body(StringBody(s"""{ "title": "${newTitle()}", "isbn": ${newIsbn()}}""")))
-      .body(StringBody("{ \"title\": \"" + newTitle() + "\", \"isbn\": " + Random.nextInt(Integer.MAX_VALUE) + " }")))
+    .exec(
+      http("create_book")
+        .post("/books")
+        .notSilent
+        .header("Content-Type", "application/json")
+        .body(StringBody("{ \"title\": \"${newTitle}\", \"isbn\": ${newIsbn} }"))
+        .check(status.is(201))
+    )
     .pause(1)
 
     .exec(http("get_books_paginated")
       .get("/books?page=1&size=20")
-
+      .check(status.is(200))
     )
 
-  setUp(scn.inject(atOnceUsers(100))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+  // setUp(scn.inject(rampUsers(3).over(3))).protocols(httpProtocol)
+
+  //
+
+  def newIsbn() = Random.nextInt(Integer.MAX_VALUE)
+
+  def newTitle() = RandomString.getInstance().randomAlpha(10)
 
 }
